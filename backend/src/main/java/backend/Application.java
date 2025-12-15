371bd5cf8b36df8cb9a8d8571914a28a20929c3d
hyun <jaehyoen3@gmail.com>
2025-12-15 23:26:09 +0900

    user-login entity 생성

--- backend/src/main/java/backend/Application.java
+++ backend/src/main/java/backend/Application.java
@@ -2,7 +2,9 @@ package backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // Entity에 시간 자동 저장하기 위한 어노테이션
 @SpringBootApplication
 public class Application {
 
